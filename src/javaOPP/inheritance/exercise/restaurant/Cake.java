9fc510e209debc6b27ce9ec44ec531cef476fb9b
NikolayNoev <n.p.noev@gmail.com>
2024-12-12 15:49:20 +0200

    homework

--- src/javaOPP/inheritance/exercise/restaurant/Cake.java
+++ src/javaOPP/inheritance/exercise/restaurant/Cake.java
@@ -3,12 +3,12 @@ package restaurant;
 import java.math.BigDecimal;
 
 public class Cake extends Dessert {
-    private static final double CAKE_GRAMS = 250;
-    private static final double CAKE_CALORIES = 1000;
-   private static final BigDecimal CAKE_PRICE = BigDecimal.valueOf(5);
+    private final static double CAKE_GRAMS = 250;
+    private final static double CAKE_CALORIES = 1000;
+    private final static BigDecimal CAKE_PRICE = BigDecimal.valueOf(5);
 
 
-    public Cake(String name, BigDecimal price, double grams, double calories) {
+    public Cake(String name) {
         super(name, CAKE_PRICE, CAKE_GRAMS, CAKE_CALORIES);
     }
 }
